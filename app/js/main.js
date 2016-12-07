c1ba8d4fd9750d5274e9918e4ff61532e45aafd7
Vladislav <azzazzelko@gmail.com>
2016-12-07 05:54:39 +0300

    added project-card page, 90% completed..

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,24 @@
 $(function() {
 
-  var sliderMin = parseInt( $(".slider-range").attr("data-min") ), //значение дата-атрибута мин
-      sliderMax = parseInt( $(".slider-range").attr("data-max") ); //значение дата-атрибута макс
+  /******************
+  *** slider-range **
+  ******************/
+  if ( $(".slider-range").length ) {
+    var sliderMin = parseInt( $(".slider-range").attr("data-min") ), //значение дата-атрибута мин
+        sliderMax = parseInt( $(".slider-range").attr("data-max") ); //значение дата-атрибута макс
 
-  $(".slider-range").slider({
-    range: true,
-    min: sliderMin,                    // минимум слайдера
-    max: sliderMax,                    // максимум слайдера
-    values: [ sliderMin, sliderMax ],  // значение на которое головки встанут
-    slide: function( event, ui ) {
-      $("#slider-amount").text(ui.values[0] + " м² - " + ui.values[1]+" м²");  //показываем изменение слайдера 
-    }
-  });
+        $(".slider-range").slider({
+          range: true,
+        min: sliderMin,                    // минимум слайдера
+        max: sliderMax,                    // максимум слайдера
+        values: [ sliderMin, sliderMax ],  // значение на которое головки встанут
+        slide: function( event, ui ) {
+          $("#slider-amount").text(ui.values[0] + " м² - " + ui.values[1]+" м²");  //показываем изменение слайдера 
+        }
+      });
 
-  $("#slider-amount").text(sliderMin + " м² - " + sliderMax+" м²");   //изначальные значения с дата атрибутов
+    $("#slider-amount").text(sliderMin + " м² - " + sliderMax+" м²");   //изначальные значения с дата атрибутов
+  };
 
 }());
 
