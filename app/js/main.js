04f4811ece8c46089ccc246169d31911fa8e8cfa
Vladislav <azzazzelko@gmail.com>
2016-12-08 01:06:09 +0300

    project-card ended, fixes, px to rem..

--- app/js/main.js
+++ app/js/main.js
@@ -20,5 +20,27 @@ $(function() {
     $("#slider-amount").text(sliderMin + " м² - " + sliderMax+" м²");   //изначальные значения с дата атрибутов
   };
 
+
+  /******************
+  *** scroll-top ****
+  ******************/
+  $(document).on( 'scroll', function(){
+
+    if ($(window).scrollTop() > 200) {
+      $('.scroll-top-wrapper').addClass('show');
+    } else {
+      $('.scroll-top-wrapper').removeClass('show');
+    }
+  });
+
+  $('.scroll-top-wrapper').on('click', scrollToTop);
+
+  function scrollToTop() {
+    verticalOffset = ( typeof(verticalOffset) != 'undefined' ) ? verticalOffset : 0;
+    offset = $('body').offset();
+    offsetTop = offset.top;
+    $('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+  };
+
 }());
 
